238b07e0c312899cdfe31819560883fbfa2bff02
Kulin Choksi <kulinchoksi@gmail.com>
2021-05-23 17:49:00 +0530

    Add files via upload
    Version 2 with filters and improvements

--- helper.js
+++ helper.js
@@ -1,34 +1,156 @@
-document.addEventListener('DOMContentLoaded', function () {
+// "use strict";
 
-	var cowin_website_link = document.getElementById('cowin_website');
+document.addEventListener('DOMContentLoaded', function () {
+	
+/* 	function onError(error) {
+	  console.error('Error: ', error);
+	}
+
+	function sendMessageToTabs(tabs) {
+	  for (let tab of tabs) {
+		chrome.tabs.sendMessage(
+		  tab.id,
+		  {greeting: "Hi from background script"}
+		).then(response => {
+		  console.log("Message from the content script:");
+		  console.log(response.response);
+		}).catch(onError);
+	  }
+	}
+
+	chrome.browserAction.onClicked.addListener(() => {
+	  chrome.tabs.query({
+		currentWindow: true,
+		active: true
+	  }).then(sendMessageToTabs).catch(onError);
+	}); */
+
+/* 	var cowin_website_link = document.getElementById('cowin_website');
 	
 	cowin_website_link.addEventListener('click', function () {
 		chrome.tabs.create({url: cowin_website_link.href});
 		return false;
 	}, false);
 
-	document.getElementById('automate_find_select_book_appointment').addEventListener('click', function () {
+	var CoWINTabFound = false;
+	async function getCoWINTab() {
+	  let queryOptions = { url: "https://selfregistration.cowin.gov.in/appointment" };
+	  let [tab] = await chrome.tabs.query(queryOptions);
+	  return tab;
+	}
+	
+	
+	getCoWINTab()
+	.then(function (tab) { // success
+		console.log("CoWINTab: " + tab + tab.length);
+		CoWINTabFound = true;
+	})
+	.catch(function (error) { // failure
+		CoWINTabFound = false;
+		console.log("CoWIN website is not opened!");
+	}); */
+	
+	// try this
+	// chrome.tabs.executeScript(tab.id,{file: "buy.js"});
+	// injects the javascript
+    // chrome.tabs.executeScript(null,{file: "buy.js"});
+	
+	// try this
+	// https://stackoverflow.com/questions/54181734/chrome-extension-message-passing-unchecked-runtime-lasterror-could-not-establi
+	
+	var automation_start_button = document.getElementById('automate_search_book_appointment');
+	var automation_stop_button = document.getElementById('stop_automation');
+	
+	automation_start_button.addEventListener('click', function () {
+		
+		/* chrome.tabs.query({ url: 'https://selfregistration.cowin.gov.in/*' }, tabs => {
+			if (tabs.length == 0) {
+				chrome.tabs.create({url: 'https://selfregistration.cowin.gov.in/appointment'}, function(tab) {
+					chrome.tabs.executeScript(tab.id,{file: "poup.js"});
+				});
+			} else {
+				tabs.forEach(tab => 
+					chrome.tabs.sendMessage(
+						tab.id,
+						{click: "automate_search_book_appointment"},
+						function(response) {
+							console.log(response);
+						}
+					)
+				);
+			}
+		}); */
+		
+		let selected_age = document.querySelector("input[name=age]:checked").value;
+		var selected_vaccines = [];
+		document.querySelectorAll("input[name=vaccine]:checked").forEach(function(selected_vaccine) {
+			selected_vaccines.push(selected_vaccine.value);
+		});
+		let request = {
+				click: "automate_search_book_appointment",
+				age: selected_age,
+				vaccines: selected_vaccines
+		};
+		
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {click: "automate_find_select_book_appointment"}, function(response) {
-				console.log(response);
+			chrome.tabs.sendMessage(tabs[0].id, request, function(response) {
+				if (typeof response !== 'undefined') {
+					console.log(response);
+					
+					automation_start_button.style.display = 'none';
+					automation_stop_button.style.display = '';
+				}
 			});
 		});
 	}, false);
 
-	document.getElementById('stop_automation').addEventListener('click', function () {
+	automation_stop_button.addEventListener('click', function () {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {click: "stop_automation"}, function(response) {
-				console.log(response);
+				if (typeof response !== 'undefined') {
+					console.log(response);
+					
+					automation_start_button.style.display = '';
+					automation_stop_button.style.display = 'none';
+				}
 			});
 		});
 	}, false);
 	
-	document.getElementById('select_and_book_appointment').addEventListener('click', function () {
+	/* document.getElementById('select_and_book_appointment').addEventListener('click', function () {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {click: "select_and_book_appointment"}, function(response) {
-				console.log(response);
+				if (typeof response !== 'undefined') {
+					console.log(response);
+				}
 			});
 		});
-	}, false);
+	}, false); */
 	
 }, false);
+
+/* var _port;
+
+function testConnect() {
+  _port.postMessage({
+    msg: 'hello from popup'
+  });
+}
+
+chrome.runtime.onConnect.addListener(function (port) {
+  console.log('connected to: ', port.name);
+  _port = port;
+
+  _port.onMessage.addListener(processMessages);
+  p_portrt.postMessage({
+    msg: 'hello from popup'
+  });
+});
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#testConnect').addEventListener(
+      'click', testConnect);
+  });
+})();
+ */
--- popup.js
+++ popup.js
@@ -1,18 +1,24 @@
+// "use strict";
 
 var automate_find_and_book;
-function automate_find_select_book_appointment() {
+var selected_age;
+var selected_vaccines;
+var slot_found = false;
+
+function automate_search_book_appointment() {
 	clearInterval(automate_find_and_book);
 	automate_find_and_book = setInterval(keep_search_select, 2000);
 	console.log("Fresh start for automated booking.");
 }
 
 function keep_search_select() {
-	// search slots
-	let search_button = document.querySelector("ion-button.pin-search-btn.district-search");
+	// find pin or district search button
+	let search_button = document.querySelector("ion-button.pin-search-btn"); // .district-search
 	if (!search_button) {
 		console.log("Search button not found!");
 		return;
 	}
+	// console.log("Search button: ", search_button);
 	
 	search_button.click();
 	console.log("Initiate search.");
@@ -22,33 +28,53 @@ function keep_search_select() {
 	setTimeout(find_and_select_available_slot, 1500);
 }
 
+function vaccinationCentreOffersSelectedVaccine(vaccine_offered_in_available_vaccination_centre) {
+	if (selected_vaccines.length > 0) {
+		console.log("Vaccine offered in available vaccination centre: ", vaccine_offered_in_available_vaccination_centre);
+		if (selected_vaccines.includes(vaccine_offered_in_available_vaccination_centre)) {
+			console.log("Found selected vaccine.");
+			return true;
+		} else {
+			console.log("Selected vaccine not found.");
+			return false;
+		}
+	} else {
+		console.log("No vaccine selected by user means anyone is fine.");
+		return true;
+	}
+}
+
 function find_and_select_available_slot() {
-	console.log("Initiate scanning and selecting available slot");
+	console.log("Initiate scanning available slot with " + selected_age + " and selected vaccines: " + selected_vaccines);
 	
-	var slot_found = false;
+	slot_found = false;
 	
-	unbooked_vaccination_centres = document.querySelectorAll("div.mat-list-text div.slots-box:not(.no-available):not(.no-seat)");
-	if (!unbooked_vaccination_centres) {
+	available_vaccination_centres = document.querySelectorAll("div.mat-list-text div.slots-box:not(.no-available):not(.no-seat)");
+	if (!available_vaccination_centres) {
 		console.log("No available slot found!");
 		return;
 	}
-	console.log("Total available slots found: " + unbooked_vaccination_centres.length);
+	console.log("Total available slots found: " + available_vaccination_centres.length);
 	
-	unbooked_vaccination_centres.forEach(function(unbooked_vaccination_centre){
-		console.log('unbooked_vaccination_centre: ', unbooked_vaccination_centre);
-		if (unbooked_vaccination_centre.querySelector("span.age-limit").textContent == "Age 18+") {
+	available_vaccination_centres.forEach(function(available_vaccination_centre){
+		console.log('Available vaccination centre: ', available_vaccination_centre);
+		vaccine_offered_in_available_vaccination_centre = available_vaccination_centre.querySelector("h5.name").textContent;
+		
+		if (available_vaccination_centre.querySelector("span.age-limit").textContent == selected_age &&
+				vaccinationCentreOffersSelectedVaccine(vaccine_offered_in_available_vaccination_centre)) {
+			
 			slot_found = true;
 			clearInterval(automate_find_and_book);
 			console.log("Stop searching further.");
 			
-			unbooked_vaccination_centre.click();
-			console.log('Selected the very fist available slot.');
+			available_vaccination_centre.click();
+			console.log('Selected the very fist available slot with selected criteria.');
 			
 			// appointment page
 			document.querySelector("div.time-slot-list ion-button").click();
 			console.log("Selected the very first time slot.");
 			
-			// refresh captcha to avoid delay because of captcha session expired
+			// Don't know why but everytime I had to referesh captcha, so automating it
 			document.querySelector("div.img-wrap a").click();
 			console.log("Captcha refereshed.");
 			
@@ -56,7 +82,7 @@ function find_and_select_available_slot() {
 			console.log("Increased the size of captcha.");
 
 			// focus captcha input
-			var captcha_input = document.querySelector("div.input-wrap input");
+			let captcha_input = document.querySelector("div.input-wrap input");
 			captcha_input.focus();
 			console.log("captcha_input: ", captcha_input);
 
@@ -84,27 +110,32 @@ function stop_automation() {
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
+    /* console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
-                "from the extension");
+                "from the extension"); */
 				
-    if (request.click == "automate_find_select_book_appointment") {
-		sendResponse({automation: "initiated"}, response => {
-			if (chrome.runtime.lastError) {
-				console.log("This solution will work on Co-WIN website only.");
-				alert("This solution will work on Co-WIN website only.");
-			}
-		});
-		
-		automate_find_select_book_appointment();
+	// check connection
+/* 	if (request.hasOwnProperty('greeting')) {
+		console.log("Message from the background script:");
+		console.log(request.greeting);
+		return Promise.resolve({response: "Hi from content script"});
+	} */
+				
+    if (request.click == "automate_search_book_appointment") {
+		selected_age = request.age;
+		selected_vaccines = request.vaccines;
+		automate_search_book_appointment();
+		sendResponse({automation: "initiated"});
 	} else if (request.click == "stop_automation") {
-		sendResponse({automation: "stopped"});
 		stop_automation();
+		sendResponse({automation: "stopped"});
 	} else if (request.click == "select_and_book_appointment") {
-		sendResponse({automation: "partial"});
 		find_and_select_available_slot();
+		sendResponse({automation: "scan_and_select_appointment"});
 	} else {
-		return;
+		/* return; */
 	}
+
+	return true;
   }
 );
